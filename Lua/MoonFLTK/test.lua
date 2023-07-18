cc8e2f24580f37b3b95ca854b19f82200cf9dcce
max00217 <makpung00217@gmail.com>
2023-07-18 15:10:40 +0900

    5.4 FLTK

--- Lua/MoonFLTK/test.lua
+++ Lua/MoonFLTK/test.lua
@@ -1,11 +1,15 @@
 fl = require("moonfltk")
 
 win = fl.window(1280, 960, arg[0])
-box = fl.box(640, 480, 300, 100, "MoonflTK Test + 저기 한국어 지원 하십니까?, 아 대굴빡이");
+box = fl.box(490, 430, 300, 100, "한국어를 지원하는 나");
 box:box('up box')
-box:labelfont(fl.BOLD + fl.ITALIC)
+box:labelfont(--[[fl.BOLD + fl.ITALIC]])
 box:labelsize(36)
 box:labeltype('shadow')
+button = fl.button(540, 600, 200, 40, "클릭이 가능한 버튼")
+button:callback(function()
+  fl.message("뭉탱이로 클릭되다")
+end)
 win:done() -- 'end' is a keyword in Lua
 win:show(arg[0], arg)
 
