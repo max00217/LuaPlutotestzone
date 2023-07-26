38986d7dc60b3d174182ff2c604991385d65d630
max00217 <makpung00217@gmail.com>
2023-07-26 17:18:38 +0900

    something was wrong

--- Lua/MoonFLTK/test.lua
+++ Lua/MoonFLTK/test.lua
@@ -3,7 +3,7 @@ fl = require("moonfltk")
 win = fl.window(1280, 960, arg[0])
 box = fl.box(490, 430, 300, 100, "한국어를 지원하는 나");
 box:box('up box')
-box:labelfont(--[[fl.BOLD + fl.ITALIC]])
+box:labelfont( --[[fl.BOLD + fl.ITALIC]])
 box:labelsize(36)
 box:labeltype('shadow')
 button = fl.button(540, 600, 200, 40, "클릭이 가능한 버튼")
