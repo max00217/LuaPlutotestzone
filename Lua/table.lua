75129ec81f41f72d36f0fc86bcec321cbe423f61
max00217 <makpung00217@gmail.com>
2023-07-29 17:10:12 +0900

    table

---
+++ Lua/table.lua
@@ -0,0 +1,8 @@
+local tbl = {"a", "c", "b", "d", aa = "zz", "e", kqcll = "asdfdnnd", ppqknv = 12}
+for k,v in ipairs(tbl) do 
+    print(k,v) 
+end
+print("===========")
+for k,v in pairs(tbl) do 
+    print(k,v) 
+end
