955e771f3f533b95f511ab9ada3860899531335f
max00217 <makpung00217@gmail.com>
2023-08-02 00:37:10 +0900

    라피스 sql 필요함

--- Lua/lapis/app.lua
+++ Lua/lapis/app.lua
@@ -1,7 +1,10 @@
 local lapis = require("lapis")
+local db = require("lapis.db")
+local Model = require("lapis.db.model").Model
 local config = require("lapis.config").get()
-
 local app = lapis.Application()
+local MyTable = Model:extend("my_table")
+
 app:enable("etlua")
 app.layout = require "views.layout"
 
@@ -19,5 +22,14 @@ app:get("/list", function(self)
   return { render = "list" }
 end)
 
+app:match("/dbtest", function()
+  local res = db.query("select * from my_table where id = ?", 10)
+  return "ok!"
+end)
+
+app:match("/dbmodel", function()
+  local row = MyTable:find(10)
+  return "ok!"
+end)
 
 return app
--- Lua/lapis/config.lua
+++ Lua/lapis/config.lua
@@ -4,6 +4,15 @@ config("development", {
   server = "nginx",
   code_cache = "off",
   num_workers = "1",
-  port = 9090,
-  greeting = "Hello world"
+  port = 5000,
+  greeting = "Lapis with LuaJIT",
+  -- sqlite = {
+  --   database = "my_database.sqlite",
+  -- }
+  mysql = {
+    host = "127.0.0.1",
+    user = "mysql_user",
+    password = "the_password",
+    database = "my_database"
+  }
 })
