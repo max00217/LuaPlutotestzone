4a7e91a649eeb8bb556549d32162a7b5549d3ee7
max00217 <makpung00217@gmail.com>
2023-09-25 11:01:55 +0900

    Bubblesort edit

--- Lua/algorithm/bubblesort.lua
+++ Lua/algorithm/bubblesort.lua
@@ -1,26 +1,14 @@
--- function bubbleSort(arr)
---     local n = #arr
---     for i = 1, n-1 do
---         for j = 1, n-i do
---             if arr[j] > arr[j+1] then
---                 arr[j], arr[j+1] = arr[j+1], arr[j]
---             end
---         end
---     end
--- end
+math.randomseed(os.time())
 
--- local arr = {5, 2, 8, 4, 7}
--- bubbleSort(arr)
--- print(table.concat(arr, ", "))
-
-local sorter = {1, 5, 7, 2, 3, 9, 10, 4, 5}
-
+local sorter = {math.random(1, 10), math.random(1, 10), math.random(1, 10), math.random(1, 10), math.random(1, 10), math.random(1, 10), math.random(1, 10), math.random(1, 10), math.random(1, 10)}
+print(table.concat(sorter, ", "))
 for i = 1, #sorter-1 do
     for j = 1, #sorter-i do
         if sorter[j] > sorter[j+1] then
             sorter[j], sorter[j+1] = sorter[j+1], sorter[j]
+            print("sorting... "..table.concat(sorter, ", "),"\n")
         end
     end
 end
 
-print(table.concat(sorter, ", "))
+print("Result: "..table.concat(sorter, ", "))
